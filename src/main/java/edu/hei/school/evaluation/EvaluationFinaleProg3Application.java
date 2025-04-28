45d73fd89561a9351619649a7b6d30b8efbb482d
Fanh2024 <hei.fanhasina@gmail.com>
2025-04-28 12:25:38 +0300

    creation projet

---
+++ src/main/java/edu/hei/school/evaluation/EvaluationFinaleProg3Application.java
@@ -0,0 +1,13 @@
+package edu.hei.school.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationFinaleProg3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationFinaleProg3Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hei/school/evaluation/EvaluationFinaleProg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hei.school.evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationFinaleProg3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
